8a06fdb9aa68b07fd50ea3ea5d6342bb946d8abb
Etto Salomons <etto.salomons@gmail.com>
2018-08-29 13:23:26 +0200

    redos en speed test

--- parseExperimentResults.py
+++ parseExperimentResults.py
@@ -2,67 +2,131 @@ import matplotlib.pyplot as plt
 import os
 from util import createPlotHtml
 
-# fileName = 'plots/20180817_train_Studio.txt'
-# pattern = '# 20180817_orgStudio'
-experimentId = '20180720_allOrgs'
-experimentId = '20180820_orgStudio_dropout'
-experimentId = '20180821_orgStudio_G428'
 
+def doMain():
+#     doMainRegular()
+    parseRedoExperiment()
+        
+def doMainRegular():
+    # fileName = 'plots/20180817_train_Studio.txt'
+    # pattern = '# 20180817_orgStudio'
+    experimentId = '20180822_StudioModel_norm'
 
-fileName = 'plots/{}.txt'.format(experimentId)
-pattern = '# ' + experimentId
+    fileName = 'plots/{}.txt'.format(experimentId)
+    pattern = '# ' + experimentId
 
-targetPlotFolder = 'plots/' + pattern[2:] + '/'
-if not os.path.isdir(targetPlotFolder):
-    os.mkdir(targetPlotFolder)
+    targetPlotFolder = 'plots/' + pattern[2:] + '/'
+    if not os.path.isdir(targetPlotFolder):
+        os.mkdir(targetPlotFolder)
 
-print('creating plots in ' + targetPlotFolder)
+    print('creating plots in ' + targetPlotFolder)
+
+    rooms = []
+    features = ['F1 overall', 'F1 music', 'F1 voice', 'F1 environment']
+
+    for feature in features:
+        roomExperiments = {
+            'G428': {},
+            'G527': {},
+            'Studio': {},
+            'Overall': {}
+        }
+        room = ''
+
+        with open(fileName) as resFile:
+            expName = ''
+            epochNr = 0
+            for line in resFile:
+                if 'testData_' in line:
+                    room = line.split('_')[1][:-1]
+                elif pattern in line:
+                    splt = line.split('ep')
+                    expName = splt[0][2:]
+                    epochNr = int(splt[1])
+                elif feature in line:
+                    if not expName in roomExperiments[room]:
+                        roomExperiments[room][expName] = []
+                    FValue = float(line.split(': ')[1])
+                    roomExperiments[room][expName].append((epochNr, FValue))
+
+        for room in roomExperiments:
+            experiments = roomExperiments[room]
+            plt.figure()
+            for exp in experiments:
+                values = sorted(experiments[exp], key=lambda tup: tup[0])
+                unzipped = list(zip(*values))
+
+                plt.plot(unzipped[0], unzipped[1], label=exp)
+
+            plt.title('Room: ' + room)
+            plt.ylabel(feature)
+            plt.xlabel('epochs')
+            plt.ylim([0.8, 1])
+            plt.legend()
+
+            figname = targetPlotFolder + pattern[2:] + '_' + room + '_' + feature + '.png'
+            plt.savefig(figname, bbox_inches='tight', dpi=200)  # 600
+
+    createPlotHtml.createPlotsInDir('plots/' + experimentId)
+        
+def parseRedoExperiment(experimentId='20180720_allOrgs',filePattern='20180823_redoAllOrgs'):
+    fileName = 'plots/{}.txt'.format(filePattern)
+    pattern = '# ' + experimentId
+
+    targetPlotFolder = 'plots/{}/'.format(filePattern)
+    if not os.path.isdir(targetPlotFolder):
+        os.mkdir(targetPlotFolder)
+
+    print('creating plots in ' + targetPlotFolder)
+
+    rooms = []
+    features = ['F1 overall', 'F1 music', 'F1 voice', 'F1 environment']
+
+    for feature in features:
+        roomExperiments = {
+            'G428': {},
+            'G527': {},
+            'Studio': {},
+            'Overall': {}
+        }
+        room = ''
+
+        with open(fileName) as resFile:
+            expName = ''
+            epochNr = 0
+            for line in resFile:
+                if 'testData_' in line:
+                    room = line.split('_')[1][:-1]
+                elif pattern in line:
+                    splt = line.split('ep')
+                    expName = splt[0][2:]
+                    epochNr = int(splt[1])
+                elif feature in line:
+                    if not expName in roomExperiments[room]:
+                        roomExperiments[room][expName] = []
+                    FValue = float(line.split(': ')[1])
+                    roomExperiments[room][expName].append((epochNr, FValue))
+
+        for room in roomExperiments:
+            experiments = roomExperiments[room]
+            plt.figure()
+            for exp in experiments:
+                values = sorted(experiments[exp], key=lambda tup: tup[0])
+                unzipped = list(zip(*values))
+
+                plt.plot(unzipped[0], unzipped[1], label=exp)
+
+            plt.title('Room: ' + room)
+            plt.ylabel(feature)
+            plt.xlabel('epochs')
+            plt.ylim([0.8, 1])
+            plt.legend()
+
+            figname = targetPlotFolder + pattern[2:] + '_' + room + '_' + feature + '.png'
+            plt.savefig(figname, bbox_inches='tight', dpi=200)  # 600
+
+    createPlotHtml.createPlotsInDir(targetPlotFolder[:-1])
     
-rooms = []
-features = ['F1 overall', 'F1 music', 'F1 voice', 'F1 environment']
-
-for feature in features:
-    roomExperiments = {
-        'G428': {},
-        'G527': {},
-        'Studio': {}
-    }
-    room = ''
-
-    with open(fileName) as resFile:
-        expName = ''
-        epochNr = 0
-        for line in resFile:
-            if 'testData_' in line:
-                room = line.split('_')[1][:-1]
-            elif pattern in line:
-                splt = line.split('ep')
-                expName = splt[0][2:]
-                epochNr = int(splt[1])
-            elif feature in line:
-                if not expName in roomExperiments[room]:
-                    roomExperiments[room][expName] = []
-                FValue = float(line.split(': ')[1])
-                roomExperiments[room][expName].append((epochNr, FValue))
-
-    for room in roomExperiments:
-        experiments = roomExperiments[room]
-        plt.figure()
-        for exp in experiments:
-            values = sorted(experiments[exp], key=lambda tup: tup[0])
-            unzipped = list(zip(*values))
-
-            plt.plot(unzipped[0], unzipped[1], label=exp)
-
-        plt.title('Room: ' + room)
-        plt.ylabel(feature)
-        plt.xlabel('epochs')
-        plt.ylim([0.8, 1])
-        plt.legend()
-
-        figname = targetPlotFolder + pattern[2:] + '_' + room + '_' + feature + '.png'
-        plt.savefig(figname, bbox_inches='tight', dpi=200)  # 600
-
 def version1():
     fileFolder = '/Users/etto/Desktop/trainResults/'
     pattern = '# 20180720_allOrgs'
@@ -110,4 +174,5 @@ def version1():
             plt.savefig(figname, bbox_inches='tight', dpi=200)  # 600
 # plt.show()
 
-createPlotHtml.createPlotsInDir('plots/' + experimentId)
+if __name__ == '__main__':
+    doMain()
--- util/createPlotHtml.py
+++ util/createPlotHtml.py
@@ -7,6 +7,46 @@ def doMain():
     createPlotsInDir('plots/20180820_orgG428_dropout')
 
 def createPlotsInDir(plotDirsLocation):
+    nrPicsInRow = 4
+    filenames = []
+    for entry in os.walk(plotDirsLocation):
+        filenames += entry[2]
+    # filenames = [fn for fn in filenames if '.png' in fn and 'Gunshot' not in fn]
+    filenames = [fn for fn in filenames if '.png' in fn]
+    filenames = sorted(filenames)
+    stepsize = len(filenames) // 4
+    #ziplist = zip(filenames[0:stepsize], filenames[stepsize:2*stepsize], filenames[2*stepsize:3*stepsize])
+    ziplist = zip(filenames[0:stepsize], filenames[stepsize:2*stepsize], 
+                  filenames[2*stepsize:3*stepsize], filenames[3*stepsize:4*stepsize])
+
+    htmlFilename = plotDirsLocation + '/overview.html'
+    with open(htmlFilename, 'w') as htmlFile:
+        titlename = (plotDirsLocation.split('/'))[-1]
+        htmlFile.write('<html><head><title>' + titlename + '</title>\n')
+        htmlFile.write('<body><H1>' + titlename + '</H1>\n')
+        htmlFile.write('\n<table width="100%">')
+        htmlFile.write('\n<STYLE TYPE="text/css">\n'
+                       '<!--\n'
+                       'TD{font-family: Arial; font-size: 16pt; font-weight:bold;}\n'
+                       '--->\n'
+                       '</STYLE>\n')
+        body = ''
+        for (one,two,three,four) in ziplist:
+            body += '\n<tr>'
+            body += '\n\t<td><img src="' + three \
+                      + '" style="width:100%;"/></td>'
+            body += '\n\t<td><img src="' + one \
+                      + '" style="width:100%;"/></td>'
+            body += '\n\t<td><img src="' + two \
+                      + '" style="width:100%;"/></td>'
+            body += '\n\t<td><img src="' + four \
+                      + '" style="width:100%;"/></td>'
+            body += '\n</tr>'
+
+        htmlFile.write(body)
+        htmlFile.write('\n</table>\n</body></html>' + '\n')
+
+def createPlotsInDirOld(plotDirsLocation):
     nrPicsInRow = 3
     filenames = []
     for entry in os.walk(plotDirsLocation):
@@ -16,7 +56,7 @@ def createPlotsInDir(plotDirsLocation):
     filenames = sorted(filenames)
     stepsize = len(filenames) // 3
     ziplist = zip(filenames[0:stepsize], filenames[stepsize:2*stepsize], filenames[2*stepsize:3*stepsize])
-
+   
     htmlFilename = plotDirsLocation + '/overview.html'
     with open(htmlFilename, 'w') as htmlFile:
         titlename = (plotDirsLocation.split('/'))[-1]
@@ -40,8 +80,7 @@ def createPlotsInDir(plotDirsLocation):
             body += '\n</tr>'
 
         htmlFile.write(body)
-        htmlFile.write('\n</table>\n</body></html>' + '\n')
-
+        htmlFile.write('\n</table>\n</body></html>' + '\n') 
 
 
 
